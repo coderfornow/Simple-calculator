377c07e556beab91e16037b6cc4beeb3075db1b9
Deathstar427 <70877885+Deathstar427@users.noreply.github.com>
2020-10-10 16:43:05 +0530

    Add files via upload

---
+++ calc/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{ int choice,x,y,z;
+    printf("enter 1st number");
+    scanf("%d",&x);
+    printf("enter 2nd number");
+    scanf("%d",&y);
+    printf("press 0 to add, 1 to sub,2 to multiply,3to divide,4 to find remainder");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 0: z=x+y;
+                printf("%d",z);
+                break;
+
+        case 1: z=x-y;
+                printf("%d",z);
+                break;
+        case 2: z=x*y;
+                printf("%d",z);
+                break;
+        case 3: z=x/y;
+                printf("%d",z);
+                break;
+        case 4: z=x%y;
+                printf("%d",z);
+                break;
+
+    }
+    getch();
+    return 0;
+}
